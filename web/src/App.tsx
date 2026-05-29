2a952f75e08747713de39af85c02a4d8377b221a
Noah Hanford (spaced) <spaced@csh.rit.edu>
2026-05-28 23:52:08 -0400

    updated frontend packages

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,8 +2,8 @@ import { useState } from 'react';
 // import reactLogo from './assets/react.svg';
 // import viteLogo from '/vite.svg';
 import './App.css';
-import 'csh-material-bootstrap/dist/css/csh-material-bootstrap.css';
-import 'csh-material-bootstrap/dist/js/color-modes.js';
+import 'csh-material-bootstrap/css';
+import 'csh-material-bootstrap/color-modes';
 import { Button, Navbar, NavbarBrand } from 'reactstrap';
 import HomeNavbar from './components/Navbar';
 
